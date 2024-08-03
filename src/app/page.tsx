934214083f4b7cee97e57960709fded7e8a66cb7
waamer <108493771+Waamer@users.noreply.github.com>
2024-08-02 21:21:21 -0400

    Image/Layout fixing and added mobile horizontal scroll indicator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,13 @@ import { LoadingScreen } from "./loading-screen";
 import { Navbar } from "./navbar"
 import { TextFade } from "../components/text-fader";
 import { DisplayModel } from "@/components/3DModel/DisplayModel";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { SiReact, SiThreedotjs, SiTypescript } from "react-icons/si";
 import { CustomCursor } from "@/components/CustomCursor";
 import { LuConstruction, LuExternalLink } from "react-icons/lu";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export default function Home() {
   const [loadingComplete, setLoadingComplete] = useState(false);
@@ -32,6 +33,10 @@ export default function Home() {
     };
   }, []);
 
+  const scrollRef = useRef(null);
+  const { scrollXProgress } = useScroll({ container: scrollRef });
+  const translateX = useTransform(scrollXProgress, [0, 1], ['0%', '100%']);
+
   return (
     <>
       {(isCursorEnabled && loadingComplete) && <CustomCursor brandName={brandName} cursorType={cursorType} />}
@@ -42,7 +47,7 @@ export default function Home() {
         id="home"
         data-observe
         className="scroll-mt-[80px] w-screen h-screen flex flex-col justify-start items-center px-4 z-[12]"
-        style={{ height: `calc(100dvh - var(--navbar-height))` }}
+        style={{ height: `calc(100vh - var(--navbar-height))` }}
       >
         <div id="home" className="absolute -mt-[var(--navbar-height)]"></div>
         <div className="select-none" onMouseEnter={() => setCursorType('text')} onMouseLeave={() => setCursorType('default')}>
@@ -52,14 +57,14 @@ export default function Home() {
         {loadingComplete && <Assistant />}
         <DisplayModel />
       </section>
-      <section id="about" data-observe className="scroll-mt-[98px] smH:mt-32 xsH:mt-52 flex flex-col justify-center items-center w-screen pb-5 px-4">
+      <section id="about" data-observe className="scroll-mt-[98px] smH:mt-32 xsH:mt-52 smH:sm:mt-0 flex flex-col justify-center items-center w-screen pb-5 px-4">
         <div className="max-w-4xl grid grid-cols-1 sm:grid-cols-3 gap-4">
           <div className="bg-black/20 backdrop-blur-[1px] p-6 rounded-lg sm:col-span-3 lg:col-span-2 flex justify-center items-center">
             <p className="text-xl leading-snug font-medium text-[#80FFDB]">
               My tech journey started off&nbsp;
               <span className="text-[#FFFFF0]">
                 with being amazed by Java and the Minecraft mods made by it as a kid, to now being a 3rd-year Computer Science student studying at&nbsp;
-                <span className="bg-purple-400 py-0.5 px-1 text-lg rounded-sm text-black font-semibold text-nowrap">Western University</span>
+                <span className="bg-purple-400 py-0.5 px-1 text-lg rounded-sm text-black font-semibold text-nowrap whitespace-nowrap">Western University</span>
                 &nbsp;who is eager to learn more about the different fields in Computer Science, hopefully, to make positive changes someday!
               </span>
             </p>
@@ -67,16 +72,16 @@ export default function Home() {
           <div className="bg-black/20 backdrop-blur-[1px] flex flex-col items-center justify-center p-6 pb-4 rounded-lg">
             <Image
               alt={'picture of me in a suit!'}
-              src={'/selfie2.jpg'}
+              src={'/selfie.png'}
               width={250}
-              height={400}
+              height={250}
               className="rounded-lg"
             />
             <p className="text-sm mt-1 leading-snug font-medium text-[#FFFFF0]">
               Proof that I look like my 3D Model!
             </p>
           </div>
-          <div className="grid grid-flow-col grid-rows-2 overflow-x-auto sm:flex flex-wrap gap-4 items-center sm:justify-center sm:col-span-2 lg:col-span-3">
+          <div ref={scrollRef} className="grid grid-flow-col grid-rows-2 overflow-x-auto sm:flex flex-wrap gap-4 items-center sm:justify-center sm:col-span-2 lg:col-span-3">
                 <a target='_blank' href="https://java.com/" className="bg-[#FFFFF0] p-6 size-24 rounded-lg"
                   onMouseEnter={() => {setBrandName("Java"); setCursorType('brand')}}
                   onMouseLeave={() => {setBrandName(undefined); setCursorType('default')}}
@@ -144,9 +149,13 @@ export default function Home() {
                   <i className="ci ci-django ci-3x"></i>
                 </a>
           </div>
+          <div className="w-2/4 h-2 rounded-[4px] mx-auto bg-black/20 sm:hidden -mb-6">
+            <motion.div className="h-2 w-2/4 rounded-[4px] bg-black/20" style={{ x: translateX }}>
+            </motion.div>
+          </div>
         </div>
       </section>
-      <section id="projects" data-observe className="scroll-mt-[80px] flex flex-col justify-center items-center w-screen py-7 md:mt-20 px-4">
+      <section id="projects" data-observe className="scroll-mt-[80px] flex flex-col justify-center items-center w-screen py-7 md:mt-14 px-4">
         <div className="grid max-w-6xl md:grid-cols-2 gap-3">
           <div className="group bg-black/20 hover:bg-[#2A9D8F]/30 p-4 rounded-lg flex flex-col sm:flex-row-reverse gap-2.5 transition-all duration-200"
             onMouseEnter={() => {setBrandName("In Progress"); setCursorType('brand')}}

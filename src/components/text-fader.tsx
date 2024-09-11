659c618549c20d6a8bacebcc81da8486c69d3ccc
waamer <108493771+Waamer@users.noreply.github.com>
2024-09-11 12:20:09 -0400

    ScrollBar design fix and finally fixed text fader

--- src/components/text-fader.tsx
+++ src/components/text-fader.tsx
@@ -25,7 +25,7 @@ export function TextFade() {
                     initial={{ opacity: 0, y: 8, filter: 'blur(8px)' }}
                     animate={{ opacity: 1, y: 0, filter: 'blur(0px)' }}
                     exit={{ opacity: 0, y: -8, filter: 'blur(8px)' }}
-                    transition={{ ease: 'easeInOut', duration: 0.6 }}
+                    transition={{ ease: 'easeInOut', duration: 0.8 }}
                     className="absolute w-full"
                 >
                     <h1 className="text-6xl font-medium text-[#E9C46A]">{currentWord}</h1>
